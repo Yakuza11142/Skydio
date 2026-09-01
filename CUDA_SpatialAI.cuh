a790396a9cf5b7540005c69f18f05405333d4724
Yakuza11142 <yakubupeter785@gmail.com>
2026-09-01 21:08:27 +0100

    Update CUDA_SpatialAI.cuh

--- CUDA_SpatialAI.cuh
+++ CUDA_SpatialAI.cuh
@@ -4,64 +4,63 @@
 #include <cuda_runtime.h>
 #include <cstdint>
 
-// Physical hardware configuration constants mapped directly to optical sensor layer registers
-constexpr uint32_t PHOTO_SENSOR_CHANNELS     = 32768; 
-constexpr uint32_t GALOIS_FIELD_ATTN_HEADS   = 32;
-constexpr uint32_t HARDWARE_CAM_RIG_LIMIT    = 6;
-constexpr uint32_t ANALOG_WAVE_COMPONENTS    = 4; 
+constexpr uint32_t BLOCK_SIZE_BITS = 3; 
+constexpr uint32_t BLOCK_VOXEL_COUNT = 512; 
+constexpr uint32_t MAX_CAMERAS = 6;
+constexpr uint32_t LATENT_FEATURE_DIM = 32;
 
-enum class InstantaneousEngineToken : uint16_t {
-    ERR_WAVEFRONT_DECOHERENCE  = 0xF001,
-    ERR_GALOIS_FIELD_DIVERGE   = 0xF002,
-    ERR_METASURFACE_BREACH     = 0xF003,
-    HAL_PHOTONIC_CORE_ENGAGED  = 0x10001,
-    ZERO_DELAY_STREAM_FLUSH    = 0x10002
+#ifdef TARGET_HARDWARE_LEGACY_TX2
+  constexpr uint32_t MAX_SPARSE_BLOCKS = 1024; 
+#else
+  constexpr uint32_t MAX_SPARSE_BLOCKS = 4096; 
+#endif
+
+struct TensorRTConfig {
+    uint32_t input_width;
+    uint32_t input_height;
+    float detection_threshold;
 };
 
-struct PhotonicWavefrontState {
-    float photon_phase_angle;        
-    float amplitude_scalar[3];          
-    float polarization_tangent_u;    
-    float polarization_tangent_v;    
+struct LensDistortionCoefficients {
+    float k1; 
+    float k2;
+    float p1; 
+    float p2;
 };
 
-struct MetasurfaceProfileHAL {
-    float refractive_index_n1;
-    float refractive_index_n2;
-    float phase_shift_tolerance;
-    float polarization_cutoff;
+struct CameraExtrinsics {
+    float rotation[9];     // Fixed to 3x3 flattened array matrix
+    float translation[3];  // Fixed to 3D offset array vector
+    float fx;              
+    float fy;              
+    float cx;              
+    float cy;              
+    LensDistortionCoefficients distortion;
 };
 
-struct PhotonicRigSensorNode {
-    float OpticalAxisRotor[4];     
-    float BaseTranslationVector[3]; 
-    float focal_length_x;
-    float focal_length_y;
-    float principal_point_x;
-    float principal_point_y;
-    MetasurfaceProfileHAL metasurface;
-};
-
-struct ZeroDelayHyperparameters {
-    float analog_phase_step;
-    float galois_field_scaling_alpha;
-    float temporal_persistence_lambda;
-    float wavefront_momentum_damping;
+struct VoxelGridMetadata {
+    float resolution_meters;
+    float origin_x;
+    float origin_y;
+    float origin_z;
+    float l_occupancy;     
+    float l_free;          
+    float l_min;           
+    float l_max;           
 };
 
 extern "C" {
-    cudaError_t AllocateInstantaneousContext();
-    cudaError_t FreeInstantaneousContext();
-    const char* QueryInstantaneousEngineToken(InstantaneousEngineToken token_id);
+    cudaError_t AllocateSparseAIBuffers();
+    cudaError_t FreeSparseAIBuffers();
     
-    cudaError_t ExecutePhotonicWavefrontUpdate(
-        const float** d_analog_voltage_fields,
-        const PhotonicRigSensorNode* d_synchronized_optical_rig,
-        const ZeroDelayHyperparameters& hyper_params,
-        PhotonicWavefrontState* d_in_out_instantaneous_field,
-        uint32_t deployed_sensors,
-        uint32_t substrate_w,
-        uint32_t substrate_h
+    cudaError_t ExecuteTemporalSparseProjection(
+        const float** d_semantic_mask_ptrs,
+        const CameraExtrinsics* d_extrinsics,
+        const VoxelGridMetadata& grid_meta,
+        float* d_in_out_sparse_voxels,
+        uint32_t active_cameras,
+        uint32_t img_width,
+        uint32_t img_height
     );
 }
 
